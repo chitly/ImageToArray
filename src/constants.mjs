bb00df1cd1b73dc8cbd81558718bbc48468701ad
Nattapong Ousirimaneechai <chai-best@hotmail.com>
2021-11-21 16:45:59 +0700

    add color mapping to byte array

---
+++ src/constants.mjs
@@ -0,0 +1,35 @@
+export const COLORS = {
+  Black: "Black",
+  White: "White",
+  Green: "Green",
+  Blue: "Blue",
+  Red: "Red",
+  Yellow: "Yellow",
+  Orange: "Orange",
+};
+
+export const COLOR_PALLETTE = {
+  Black: [0, 0, 0],
+  White: [255, 255, 255],
+  Green: [0, 128, 2],
+  Blue: [8, 0, 255],
+  Red: [255, 1, 1],
+  Yellow: [255, 255, 0],
+  Orange: [255, 170, 1],
+};
+
+export const COLOR_MAPPING = {
+  Black: "0", //Black	0b0000	0x0
+  White: "1", //White	0b0001	0x1
+  Green: "2", //Green	0b0010	0x2
+  Blue: "3", //Blue	0b0011	0x3
+  Red: "4", //Red	0b0100	0x4
+  Yellow: "5", //Yellow	0b0101	0x5
+  Orange: "6", //Orange	0b0110	0x6
+};
+
+export default {
+  COLORS,
+  COLOR_PALLETTE,
+  COLOR_MAPPING,
+};
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,7 @@
 import getPixels from "get-pixels";
+import { COLORS, COLOR_PALLETTE, COLOR_MAPPING } from "./constants.mjs";
 
+// Currently the following file formats are supported: PNG, JPEG, GIF
 getPixels("https://i.imgur.com/4O8VFQY.jpg", (err, pixels) => {
   if (err) {
     console.log("Bad image path");
@@ -8,15 +10,31 @@ getPixels("https://i.imgur.com/4O8VFQY.jpg", (err, pixels) => {
   console.log("got pixels", pixels.shape);
   let w = pixels.shape[0];
   let h = pixels.shape[1];
+  let s = "0x";
+  let outArr = [];
   for (let y = 0; y < h; y++) {
     for (let x = 0; x < w; x++) {
       let r = pixels.get(x, y, 0);
       let g = pixels.get(x, y, 1);
       let b = pixels.get(x, y, 2);
       let a = pixels.get(x, y, 3);
-      console.log("pixel", x, y, r, g, b, a);
-      break;
+      // console.log("pixel", x, y, r, g, b, a);
+      let hasColor = false;
+      for (const [color, rgb] of Object.entries(COLOR_PALLETTE)) {
+        if (r === rgb[0] && g === rgb[1] && b === rgb[2]) {
+          s = `${s}${COLOR_MAPPING[color]}`;
+          hasColor = true;
+          break;
+        }
+      }
+      if (!hasColor) {
+        s = `${s}${COLOR_MAPPING[COLORS.White]}`;
+      }
+      if (s.length === 4) {
+        outArr.push(s);
+        s = "0x";
+      }
     }
-    break;
   }
+  console.log(outArr);
 });
